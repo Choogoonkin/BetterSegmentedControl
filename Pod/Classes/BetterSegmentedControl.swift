ba66c30e32d1ee4493afe09e8055ec1d36d4e466
Vladislav Chugunkin <vc@trinitydigital.ru>
2018-02-13 10:48:55 +0300

    addShadow

--- Pod/Classes/BetterSegmentedControl.swift
+++ Pod/Classes/BetterSegmentedControl.swift
@@ -154,6 +154,13 @@ import UIKit
         }
     }
     
+    public func addShadow(_ color: UIColor = .black, offset: CGSize = CGSize(width: 0.0, height: 0.0), radius: CGFloat = 10, opacity: Float = 0.15) {
+        indicatorView.layer.shadowColor = color.cgColor
+        indicatorView.layer.shadowOffset = offset
+        indicatorView.layer.shadowRadius = radius
+        indicatorView.layer.shadowOpacity = opacity
+    }
+    
     /// Whether the indicator should bounce when selecting a new index. Defaults to true
     @IBInspectable public fileprivate(set) var bouncesOnChange: Bool = true
     /// Whether the the control should always send the .ValueChanged event, regardless of the index remaining unchanged after interaction. Defaults to false
